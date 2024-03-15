691fd71246ba066d9b3378e29e7029f1c42312ff
bharat-yosa <bharat.joshi@appsontechnologies.in>
2024-03-15 14:22:41 +0530

    complete feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CreatePost from './CreatePost.js';
+import Tweet from './Tweet.js';
 export default function Feed() {
   return (
     <div>
        <div>
            <CreatePost />
+           <Tweet />
        </div>
     </div>
   )
